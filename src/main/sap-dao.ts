1d52dbb34a32263ba147d63337bf8672ae99ed16
Rana Loda Tama Lubis <rana.lubis@gmail.com>
2018-12-28 11:15:03 +0700

    fix gitignore

--- src/main/sap-dao.ts
+++ src/main/sap-dao.ts
@@ -3,14 +3,14 @@ export class SapDao
 	connector() { return (<any>this).getDataSource().connector }
 
 	find(filter, options?) {
-		return this.connector().find((<any>this).modelName, filter, options)
+		return this.connector().find((<any>this).modelName.toLowerCase(), filter, options)
 	}
 
 	findById(id, filter, options?) {
-		return this.connector().findById((<any>this).modelName, id, filter, options)
+		return this.connector().findById((<any>this).modelName.toLowerCase(), id, filter, options)
 	}
 
 	findOne(filter, options?) {
-		return this.connector().findOne((<any>this).modelName, filter, options)
+		return this.connector().findOne((<any>this).modelName.toLowerCase(), filter, options)
 	}
 }
